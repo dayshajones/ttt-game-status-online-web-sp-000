aa72680d4c3d7945cab31084eb81d5143b580953
Daysha Jones <daysha.vu@icloud.com>
2021-03-10 21:50:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,5 +54,17 @@ def over?
 end
 
 def winner
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
   
 end
